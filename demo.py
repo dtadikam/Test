dd5a19f98004d9cc21e5dcfadefe3ca7074657c1
dtadikam <50409004+dtadikam@users.noreply.github.com>
2019-09-25 18:09:50 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,13 @@
 
+import subprocess
+
+out = subprocess.Popen(['git', 'clone', 'https://github.com/dtadikam/Test.git'], 
+           stdout=subprocess.PIPE, 
+           stderr=subprocess.STDOUT)
+stdout,stderr = out.communicate()
+print(stdout)
+print(stderr)
+
+f = open("D://Users//tadikama//Desktop//Test//sample.txt", "r").readlines()
+for x in f:
+  print(x)
