2b7de7bcbdbf2b2788c2eff412d8fd58d38cd4c8
dtadikam <50409004+dtadikam@users.noreply.github.com>
2019-09-25 18:15:51 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,13 +1,4 @@
-
-import subprocess
-
-out = subprocess.Popen(['git', 'clone', 'https://github.com/dtadikam/Test.git'], 
-           stdout=subprocess.PIPE, 
-           stderr=subprocess.STDOUT)
-stdout,stderr = out.communicate()
-print(stdout)
-print(stderr)
-
-f = open("D://Users//tadikama//Desktop//Test//sample.txt", "r").readlines()
-for x in f:
-  print(x)
+import requests
+url = 'https://raw.githubusercontent.com/dtadikam/Test/master/README.md'
+req = requests.get(url)
+print(req.text)
